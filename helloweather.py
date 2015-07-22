ab1ed434cbc0fe68d3ba907381ac9d67c68653dd
Dimitrios Liappis <liappis@pythian.com>
2015-07-22 16:25:17 +0300

    Initial commit of app

---
+++ helloweather.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import urllib2
+import json
+from flask import Flask,render_template
+import pdb
+app = Flask(__name__)
+
+@app.route('/')
+def helloweather():
+    # Return my IP and lon/lat
+    req=urllib2.Request('http://ipinfo.io')
+    req.add_header('User-Agent','curl')
+    response=urllib2.urlopen(req)
+    geoip_params=""
+    try:
+        geoip_params=json.loads(response.read())
+    except:
+        print "Couldn't retrieve your IP / location"
+
+    lat,lon = geoip_params['loc'].split(',')
+    print lon,lat
+    weatherurl = "http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s" % (lat,lon)
+    weatherdata = urllib2.urlopen(weatherurl)
+    dictweatherdata=""
+    try:
+        dictweatherdata = json.loads(weatherdata.read())
+    except:
+        print "Couldn't retrieve weather in your area :("
+    return render_template('hello.html',weatherdata=dictweatherdata)
+
+if __name__ == '__main__':
+    app.run(debug=True)
